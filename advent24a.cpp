513c312d23b33f1e1ec73dceb4a936c3812cc1d8
Tomasz Kwiatkowski <quiath@gmail.com>
2018-12-25 00:56:38 +0100

    Added data, does not pass

--- advent24a.cpp
+++ advent24a.cpp
@@ -36,12 +36,37 @@ GroupVec example
     { 1, 2, 4485, 2961, Slash, 12, 4, {0, 1, 2, 1, 2}  },
 };
 
+GroupVec problem
+{
+{ 0, 1, 2086, 11953, Cold, 46, 13, {1, 1, 1, 1, 1} },
+{ 0, 2, 329, 3402, Slash, 90, 1, {1, 2, 1, 1, 1} },
+{ 0, 3, 414, 7103, Radi, 170, 4, {0, 2, 1, 1, 1} },
+{ 0, 4, 2205, 7118, Radi, 26, 18, {1, 1, 2, 1, 0} },
+{ 0, 5, 234, 9284, Radi, 287, 12, {1, 1, 0, 2, 0} },
+{ 0, 6, 6460, 10804, Slash, 15, 11 , {1, 1, 2, 1, 1} },
+{ 0, 7, 79, 1935, Radi, 244, 8 , {1, 1, 1, 1, 1} },
+{ 0, 8, 919, 2403, Slash, 22, 2, {1, 1, 2, 1, 1} },
+{ 0, 9, 172, 1439, Slash, 69, 3, {1, 1, 0, 2, 0} },
+{ 0, 10, 1721, 2792, Cold, 13, 16, {2, 1, 2, 1, 1} },
+ 
 
+{ 1, 1, 1721, 29925, Radi, 34, 5, {2, 1, 1, 0, 2} },
+{ 1, 2, 6351, 21460, Slash, 6, 15, {1, 1, 1, 1, 2} },
+{ 1, 3, 958, 48155, Radi, 93, 7, {1, 2, 1, 1, 1} },
+{ 1, 4, 288, 41029, Cold, 279, 20, {1, 0, 1, 1, 1} },
+{ 1, 5, 3310, 38913, Radi, 21, 19, {1, 1, 1, 1, 1} },
+{ 1, 6, 3886, 16567, Cold, 7, 9, {1, 0, 1, 1, 0} },
+{ 1, 7, 39, 7078, Blud, 300, 14, {1, 1, 1, 1, 1} },
+{ 1, 8, 241, 40635, Fire, 304, 6, {1, 1, 1, 1, 2} },
+{ 1, 9, 7990, 7747, Radi, 1, 10, {1, 1, 0, 1, 1} },
+{ 1, 10, 80, 30196, Blud, 702, 17, {1, 1, 2, 1, 1} }
+};
 
 struct Logic
 {
-    explicit Logic(const GroupVec g) : g_{g} {} // copy
+    explicit Logic(const GroupVec g) : g_{g}, n_{g.size()} {} // copy
     GroupVec g_;
+    size_t n_;
 
     int iter();
 
@@ -82,7 +107,7 @@ int Logic::iter()
     IntVec ato(g_.size(), -1);
     IntVec afrom(g_.size(), -1);
 
-    IntVec order(g_.size() + 1, -1);
+    IntVec order(n_ + 1, -1);
 
     for (auto i = 0; i < g_.size(); ++i)
     {
@@ -97,9 +122,13 @@ int Logic::iter()
                 name[c.team_] << " " << c.id_ << 
                 " enemyid=" << g_[j].id_ <<
                 " enemyefi=" << g_[j].getEffpower() 
-                << " dmg=" <<  totdamage << "<>" << maxdmg << " " << 
-                (gtDesired(g_[j], g_[otherid])) <<
-                " a=" << g_[j].getInit() << "<>" << g_[otherid].getInit() << endl;
+                << " dmg=" <<  totdamage << "<>" << maxdmg << " ";
+                if (otherid >= 0) 
+                {
+                    cout << (gtDesired(g_[j], g_[otherid])) <<
+                    " a=" << g_[j].getInit() << "<>" << g_[otherid].getInit();
+                }
+                cout << endl;
                 if (totdamage > maxdmg ||
                     (totdamage == maxdmg && gtDesired(g_[j], g_[otherid])
                     ))
@@ -148,12 +177,23 @@ int Logic::iter()
                  "| " << gto.id_ << " dmg=" << totdamage << " killing " << killed << " remain=" << gto.units_ << endl;
     
     }
+    cout << "round ended" << endl;
 
     g_.erase(std::remove_if(begin(g_), end(g_),
         [](const auto& a){ return a.units_ == 0;}), end(g_));
+    
+    cout << "erased" << endl;
     auto cnt_imm{std::count_if(begin(g_), end(g_), 
         [](const auto& a){ return a.team_ == 0;}
     )};
+    cout << "counted" << endl;
+
+    for (const auto& c : g_)
+    {
+        cout << "Rem:" << name[c.team_] << " " << c.id_ << 
+                 " units=" << c.units_ << endl;
+
+    }
 
     if (cnt_imm == 0) { return 1; }
     if (cnt_imm == g_.size()) { return -1; }
@@ -163,7 +203,8 @@ int Logic::iter()
 
 int main()
 {
-    Logic logic{example};
+    //Logic logic{example};
+    Logic logic{problem};
     auto ret{0};
     do 
     {
