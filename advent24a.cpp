d01987db6bb27fe07f6ef3c4e1f92876c57a3f5e
Tomasz Kwiatkowski <quiath@gmail.com>
2018-12-25 04:12:27 +0100

    Fixed input data - star 1 on day 24

--- advent24a.cpp
+++ advent24a.cpp
@@ -1,5 +1,6 @@
 #include "advent.h"
 #include <array>
+#include <regex>
 //int const BLU = 0;
 
 int const N{5};
@@ -24,6 +25,36 @@ struct Group
 
     int getEffpower() const { return units_ * damage_; }
     int getInit() const { return init_; }
+
+
+    auto getDamageInt(const Group& to) const
+    {
+        return units_ * damage_ * to.def_[attack_];
+    };
+    auto getDamage(const Group& to) const
+    {
+        //return units_ * damage_ * to.def_[attack_];
+        return getDamageInt(to);
+    };
+    int getModDamage(const Group& to) const
+    {
+        return (getDamageInt(to)/ to.hitpoints_) * to.hitpoints_;
+    };
+
+    bool operator==(const Group& other) const
+    {
+        return team_ == other.team_ &&
+        id_== other.id_ &&
+        units_== other.units_ &&
+        hitpoints_== other.hitpoints_ &&
+        attack_== other.attack_ &&
+        damage_ == other.damage_ &&
+        init_ == other.init_ && def_[0] == other.def_[0]/*&&
+        def_ == other.def_*/;
+
+
+    }
+
 };
 
 using GroupVec = std::vector<Group>;
@@ -35,6 +66,15 @@ GroupVec example
     { 1, 1, 801, 4706, Blud, 116, 1, {2, 1, 1, 1, 1}  },
     { 1, 2, 4485, 2961, Slash, 12, 4, {0, 1, 2, 1, 2}  },
 };
+
+GroupVec example1
+{
+    { 0, 1, 4, 2, Fire, 1, 1, {1, 0, 1, 1, 1}  },
+    { 1, 1, 1, 1, Blud, 1, 4, {1, 1, 1, 1, 1}  },
+    { 1, 2, 1, 1, Slash, 1, 2, {1, 1, 1, 1, 1}  },
+    { 1, 3, 1, 1, Slash, 1, 3, {1, 1, 0, 1, 1}  },
+};
+
 
 GroupVec problem
 {
@@ -53,7 +93,7 @@ GroupVec problem
 { 1, 1, 1721, 29925, Radi, 34, 5, {2, 1, 1, 0, 2} },
 { 1, 2, 6351, 21460, Slash, 6, 15, {1, 1, 1, 1, 2} },
 { 1, 3, 958, 48155, Radi, 93, 7, {1, 2, 1, 1, 1} },
-{ 1, 4, 288, 41029, Cold, 279, 20, {1, 0, 1, 1, 1} },
+{ 1, 4, 288, 41029, Cold, 279, 20, {2, 0, 1, 1, 1} },
 { 1, 5, 3310, 38913, Radi, 21, 19, {1, 1, 1, 1, 1} },
 { 1, 6, 3886, 16567, Cold, 7, 9, {1, 0, 1, 1, 0} },
 { 1, 7, 39, 7078, Blud, 300, 14, {1, 1, 1, 1, 1} },
@@ -61,6 +101,120 @@ GroupVec problem
 { 1, 9, 7990, 7747, Radi, 1, 10, {1, 1, 0, 1, 1} },
 { 1, 10, 80, 30196, Blud, 702, 17, {1, 1, 2, 1, 1} }
 };
+
+StrVec immv {
+"2086 units each with 11953 hit points with an attack that does 46 cold damage at initiative 13",
+"329 units each with 3402 hit points (weak to bludgeoning) with an attack that does 90 slashing damage at initiative 1",
+"414 units each with 7103 hit points (weak to bludgeoning; immune to radiation) with an attack that does 170 radiation damage at initiative 4",
+"2205 units each with 7118 hit points (immune to cold; weak to fire) with an attack that does 26 radiation damage at initiative 18",
+"234 units each with 9284 hit points (weak to slashing; immune to cold, fire) with an attack that does 287 radiation damage at initiative 12",
+"6460 units each with 10804 hit points (weak to fire) with an attack that does 15 slashing damage at initiative 11",
+"79 units each with 1935 hit points with an attack that does 244 radiation damage at initiative 8",
+"919 units each with 2403 hit points (weak to fire) with an attack that does 22 slashing damage at initiative 2",
+"172 units each with 1439 hit points (weak to slashing; immune to cold, fire) with an attack that does 69 slashing damage at initiative 3",
+"1721 units each with 2792 hit points (weak to radiation, fire) with an attack that does 13 cold damage at initiative 16    "
+};
+
+StrVec infv {
+"1721 units each with 29925 hit points (weak to cold, radiation; immune to slashing) with an attack that does 34 radiation damage at initiative 5",
+"6351 units each with 21460 hit points (weak to cold) with an attack that does 6 slashing damage at initiative 15",
+"958 units each with 48155 hit points (weak to bludgeoning) with an attack that does 93 radiation damage at initiative 7",
+"288 units each with 41029 hit points (immune to bludgeoning; weak to radiation) with an attack that does 279 cold damage at initiative 20",
+"3310 units each with 38913 hit points with an attack that does 21 radiation damage at initiative 19",
+"3886 units each with 16567 hit points (immune to bludgeoning, cold) with an attack that does 7 cold damage at initiative 9",
+"39 units each with 7078 hit points with an attack that does 300 bludgeoning damage at initiative 14",
+"241 units each with 40635 hit points (weak to cold) with an attack that does 304 fire damage at initiative 6",
+"7990 units each with 7747 hit points (immune to fire) with an attack that does 1 radiation damage at initiative 10",
+"80 units each with 30196 hit points (weak to fire) with an attack that does 702 bludgeoning damage at initiative 17"
+};
+
+StrVec w_vec {"radiation", "bludgeoning", "fire", "slashing", "cold"};
+
+void parse(const StrVec& sv, GroupVec& gv, int team)
+{
+    std::regex uregex{"([0-9][0-9]*) units"};
+    std::regex hregex{"([0-9][0-9]*) hit"};
+    std::regex iregex{"initiative ([0-9][0-9]*)"};
+    std::regex dregex{"([0-9][0-9]*) ([a-z][a-z]*) damage"};
+    std::regex wregex{"weak to ([a-z, ]*)"};
+    std::regex imregex{"immune to ([a-z, ]*)"};
+
+    auto id{1};
+    for (const auto& s : sv)
+    {
+        Group g;
+
+        std::smatch m;
+        if (std::regex_search(s, m, uregex))
+        {
+            g.units_ = std::stoi(m[1], nullptr);
+        }
+        else
+        {
+            throw string("units");
+        }
+        if (std::regex_search(s, m, hregex))
+        {
+            g.hitpoints_ = std::stoi(m[1], nullptr);
+        }
+        else
+        {
+            throw string("hit");
+        }
+
+        if (std::regex_search(s, m, iregex))
+        {
+            g.init_ = std::stoi(m[1], nullptr);
+        }
+        else
+        {
+            throw string("initiative");
+        }
+
+        if (std::regex_search(s, m, dregex))
+        {
+            g.damage_ = std::stoi(m[1], nullptr);
+            std::string ms{m[2]};
+            for (auto i = 0; i < w_vec.size(); ++i)
+            {
+                if (ms.find(w_vec[i]) != string::npos)
+                {
+                    g.attack_ = Attack(i);
+                }
+            }
+        }
+        else
+        {
+            throw string("damage");
+        }
+        if (std::regex_search(s, m, wregex))
+        {
+            std::string ms{m[1]};
+            for (auto i = 0; i < w_vec.size(); ++i)
+            {
+                if (ms.find(w_vec[i]) != string::npos)
+                {
+                    g.def_[i] = 2;
+                }
+            }
+        }
+        if (std::regex_search(s, m, imregex))
+        {
+            std::string ms{m[1]};
+            for (auto i = 0; i < w_vec.size(); ++i)
+            {
+                if (ms.find(w_vec[i]) != string::npos)
+                {
+                    g.def_[i] = 0;
+                }
+            }
+        }
+        g.team_ = team;
+        g.id_ = id++;
+        gv.emplace_back(g); 
+    }
+
+}
 
 struct Logic
 {
@@ -96,11 +250,11 @@ int Logic::iter()
     std::sort(begin(g_), end(g_), 
      [](auto a, auto b)
      {
-         if (a.getEffpower() == b.getEffpower())
+         if (a.team_ == b.team_)
          {
-             return a.getInit() > b.getInit();
+             return gtDesired(a, b);
          }
-         return a.getEffpower() > b.getEffpower();
+        return a.team_ < b.team_;
      });
 
 
@@ -112,14 +266,18 @@ int Logic::iter()
     for (auto i = 0; i < g_.size(); ++i)
     {
         auto& c{g_[i]};
+        if (c.units_ == 0)
+        {
+            continue;
+        }
         auto otherid{-1}, maxdmg{-1};
         for (auto j = 0; j < g_.size(); ++j)
         {
-            if (i != j && c.team_ != g_[j].team_ && afrom[j] < 0)
+            if (i != j && c.team_ != g_[j].team_ && afrom[j] < 0 && g_[j].units_ > 0)
             {
-                auto totdamage{g_[j].def_[c.attack_] * c.damage_ * c.units_};
+                auto totdamage{c.getDamage(g_[j])};
                 cout << 
-                name[c.team_] << " " << c.id_ << 
+                name[c.team_] << " " << c.id_ << "(" << c.getEffpower() << "/" << c.getInit() << ")" <<
                 " enemyid=" << g_[j].id_ <<
                 " enemyefi=" << g_[j].getEffpower() 
                 << " dmg=" <<  totdamage << "<>" << maxdmg << " ";
@@ -129,6 +287,10 @@ int Logic::iter()
                     " a=" << g_[j].getInit() << "<>" << g_[otherid].getInit();
                 }
                 cout << endl;
+                if (totdamage == 0)
+                {
+                    continue;
+                }
                 if (totdamage > maxdmg ||
                     (totdamage == maxdmg && gtDesired(g_[j], g_[otherid])
                     ))
@@ -146,6 +308,8 @@ int Logic::iter()
         }
         if (otherid >= 0 && maxdmg > 0)
         {
+            cout << 
+                name[c.team_] << " " << c.id_ << " chose " << g_[otherid].id_ << " of team " << name[g_[otherid].team_] << endl;
             ato[i] = otherid;
             afrom[otherid] = i;
             order[c.getInit()] = i;
@@ -167,7 +331,7 @@ int Logic::iter()
             continue;
         }
 
-        auto totdamage{gto.def_[c.attack_] * c.damage_ * c.units_};
+        auto totdamage{c.getDamage(gto)};
 
         auto fullunits{totdamage / gto.hitpoints_};
         auto killed{std::min(gto.units_, fullunits)};
@@ -179,14 +343,18 @@ int Logic::iter()
     }
     cout << "round ended" << endl;
 
-    g_.erase(std::remove_if(begin(g_), end(g_),
+    /*g_.erase(std::remove_if(begin(g_), end(g_),
         [](const auto& a){ return a.units_ == 0;}), end(g_));
     
-    cout << "erased" << endl;
+    cout << "erased" << endl;*/
     auto cnt_imm{std::count_if(begin(g_), end(g_), 
-        [](const auto& a){ return a.team_ == 0;}
+        [](const auto& a){ return a.team_ == 0 && a.units_ > 0;}
     )};
-    cout << "counted" << endl;
+    auto cnt_inf{std::count_if(begin(g_), end(g_), 
+        [](const auto& a){ return a.team_ == 1 && a.units_ > 0;}
+    )};
+    cout << "counted " << cnt_imm << "," <<cnt_inf << endl;
+    //21768-22811
 
     for (const auto& c : g_)
     {
@@ -196,7 +364,8 @@ int Logic::iter()
     }
 
     if (cnt_imm == 0) { return 1; }
-    if (cnt_imm == g_.size()) { return -1; }
+    if (cnt_inf == 0) { return -1; }
+    //if (cnt_imm == g_.size()) { return -1; }
     return 0;
 
 }
@@ -204,7 +373,37 @@ int Logic::iter()
 int main()
 {
     //Logic logic{example};
-    Logic logic{problem};
+    //Logic logic{problem};
+
+#if 1  
+    GroupVec parsed;
+    parse(immv, parsed, 0);
+    parse(infv, parsed, 1);
+    Logic logic{parsed};
+
+    cout << "equal " << (parsed == problem) << endl;
+
+    auto pr_ind = [](const auto& v)
+    {
+        for (auto i = 0; i < v.size(); ++i)
+        {
+            cout << v[i] << " ";
+        }
+    };
+
+    for (auto i = 0; i < parsed.size(); ++i)
+    {
+        if (!(parsed[i] == problem[i]))
+        {
+            cout << "diff " << i  << endl;
+            pr_ind(parsed[i].def_);
+            cout << endl;
+            pr_ind(problem[i].def_);
+            cout << endl;
+        }
+    }
+#endif
+
     auto ret{0};
     do 
     {
